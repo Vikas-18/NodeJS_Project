4ee6f8e721a54fa6ee9f491f9d5d945056192ac9
Vikas-18 <vikas2k24nitrr@gmail.com>
2024-09-22 15:22:18 +0530

    port changed

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ connectDB();
 app.use("", authRoutes);
 app.use("", paymentRoutes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
